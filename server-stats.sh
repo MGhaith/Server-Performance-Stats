c4d6af717c75ba19402dc4b45848485e303a42ec
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-15 18:19:40 +0100

    Add Memory Usage

--- server-stats.sh
+++ server-stats.sh
@@ -7,3 +7,7 @@ echo "=================================="
 # CPU Usage
 echo -e "\n*CPU Usage*"
 top -bn1 | grep "Cpu(s)" | awk '{print "CPU Usage: " $2 + $4 "%"}'
+
+# Memory Usage
+echo -e "\n*MEMORY USAGE*"
+free -m | awk 'NR==2{printf "Used: %s MB (%.2f%%), Free: %s MB\n", $3, $3*100/$2, $4}'
