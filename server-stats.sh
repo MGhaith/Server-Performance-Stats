0d6b57b317cf04ba7acd6089222dcb74104427b8
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-15 18:18:02 +0100

    Add CPU Usage

--- server-stats.sh
+++ server-stats.sh
@@ -3,3 +3,7 @@
 echo "=================================="
 echo "      SERVER PERFORMANCE STATS    "
 echo "=================================="
+
+# CPU Usage
+echo -e "\n*CPU Usage*"
+top -bn1 | grep "Cpu(s)" | awk '{print "CPU Usage: " $2 + $4 "%"}'
